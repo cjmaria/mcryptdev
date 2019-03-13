b16667df149b0bbdc7891da46939f68e9e294b98
Chris Maria <christopher.maria.109@gmail.com>
2019-03-13 08:57:59 -0400

    Testing dynamic memory

--- src/mgos_mcryptdev.c
+++ src/mgos_mcryptdev.c
@@ -8,19 +8,19 @@ int add(int a, int b){
     return a + b;
 }
 
-/*
+
 uint8_t generateRandom32(){
-    //uint8_t* random_number = (uint8_t*) malloc(sizeof(uint8_t));
-    uint8_t random_number[32];
+    uint8_t* random_number = (uint8_t*) malloc(sizeof(uint8_t));
+    //uint8_t random_number[32];
     atcab_init(&cfg_ateccx08a_i2c_default);
     atcab_random(random_number);
     
     return *random_number;
 }
-*/
+
 int generateRandom(){
-    //return (int) generateRandom32();
-    return 255;
+    return (int) generateRandom32();
+    //return 255;
     //RandomGenerator rg;
     //return rg.random8();
 }
