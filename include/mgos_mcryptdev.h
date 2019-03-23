9da2fd9ea985536a6d3d14a8f83e672e95465157
Chris Maria <christopher.maria.109@gmail.com>
2019-03-23 13:40:38 -0400

    Elimintating objectification

--- include/mgos_mcryptdev.h
+++ include/mgos_mcryptdev.h
@@ -16,19 +16,6 @@ int add(int a, int b);
 uint8_t generateRandom32();
 int generateRandom();
 
-/*
-class RandomGenerator{
-public:
-    RandomGenerator();
-    ~RandomGenerator();
-    void destroy();
-    int random8();
-
-private:
-    uint8_t* uint8;
-};*/
-
-
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
--- src/mgos_mcryptdev.cpp
+++ src/mgos_mcryptdev.cpp
@@ -11,25 +11,6 @@ int add(int a, int b){
 // "Converting structs to objects"
 // "https://github.com/cesanta/mjs"
 
-struct uintLayers {
-  int8_t i8;
-  int16_t i16;
-  uint8_t u8;
-  uint16_t u16;
-};
-
-/*
-static const struct mjs_c_struct_member mjsUintInterface[] = {
-  {"i8", offsetof(struct uintLayers, i8), MJS_STRUCT_FIELD_TYPE_INT8, NULL},
-  {"i16", offsetof(struct uintLayers, i16), MJS_STRUCT_FIELD_TYPE_INT16, NULL},
-  {"u8", offsetof(struct uintLayers, u8), MJS_STRUCT_FIELD_TYPE_UINT8, NULL},
-  {"u16", offsetof(struct uintLayers, u16), MJS_STRUCT_FIELD_TYPE_UINT16, NULL},
-  {NULL, 0, MJS_STRUCT_FIELD_TYPE_INVALID, NULL},
-};
-
-const struct mjs_c_struct_member *getMjsUintInterface(void) {
-  return mjsUintInterface;
-};*/
 
 uint8_t generateRandom32(){
     uint8_t* random_number = (uint8_t*) malloc(sizeof(uint8_t[32]));
@@ -48,25 +29,6 @@ int generateRandom(){
     //RandomGenerator rg;
     //return rg.random8();
 }
-/*
-
-RandomGenerator::RandomGenerator(){
-    uint8 = (uint8_t*) malloc(sizeof(uint8_t));
-}*/
-void RandomGenerator::destroy(){
-    delete uint8;
-}
-/*
-RandomGenerator::~RandomGenerator(){
-    destroy();
-}
-*/
-int RandomGenerator::random8(){
-    atcab_init(&cfg_ateccx08a_i2c_default);
-    atcab_random(uint8);
-    return (int) *uint8;
-}
-
 
 
 
