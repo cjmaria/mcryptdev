ab345fef3a734d2ede511bee83dc1f56683eb1e3
Chris Maria <christopher.maria.109@gmail.com>
2019-04-27 10:35:29 -0400

    Ensuring lib is up to date

--- mjs_fs/api_mcryptdev.js
+++ mjs_fs/api_mcryptdev.js
@@ -1,6 +1,4 @@
 load("api_i2c.js")
-// I2C address for the crypto-chip, atecc508a is usually 96
-let I2Caddr = 96;
 let mcryptdev = {
     //add: ffi('int add(int, int)'),
     //readInitBit: function(){
