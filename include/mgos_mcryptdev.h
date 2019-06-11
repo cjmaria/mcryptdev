4cb76c8b3213e2b7395e6f4e2f8c79c2b5711949
Chris Maria <christopher.maria.109@gmail.com>
2019-06-11 08:56:58 -0400

    Added fixed key of uint8_t type

--- include/mgos_mcryptdev.h
+++ include/mgos_mcryptdev.h
@@ -37,7 +37,7 @@ void * encrypt(const void * data, size_t len, size_t * out_len);
  */
 void * decrypt(const void * data, size_t len, size_t * out_len);
 
-const char *device_key = "eOe+jn5rHZ2oNKIiY2I4mxZgsfucSJM97Rd/eFis9HU=";
+const uint8_t *device_key = "eOe+jn5rHZ2oNKIiY2I4mxZgsfucSJM97Rd/eFis9HU=";
 
 bool mgos_mcryptdev_init(void);
 
